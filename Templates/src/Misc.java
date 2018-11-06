412a62b09d6ad11e8df083c9398eb4f27d5983b0
StormFalcon32 <benchen9708@gmail.com>
2018-11-06 17:58:13 -0500

    Added simple primality test to Misc, removed bitwise sieve (not useful)

--- Templates/src/Misc.java
+++ Templates/src/Misc.java
@@ -1,4 +1,3 @@
-import java.util.HashSet;
 import java.util.LinkedList;
 
 public class Misc {
@@ -37,36 +36,18 @@ public class Misc {
 		}
 		return ret;
 	}
-
-	// Bitwise sieve to check for primes
-
-	static int ifnotPrime(int prime[], int x) {
-		return (prime[x / 64] & (1 << ((x >> 1) & 31)));
-	}
-
-	static void makeComposite(int prime[], int x) {
-		prime[x / 64] |= (1 << ((x >> 1) & 31));
-	}
-
-	static HashSet<Integer> bitwiseSieve(int n) {
-		// All primes <= n
-		int prime[] = new int[n / 64 + 1];
-		for (int i = 3; i * i <= n; i += 2) {
-			if (ifnotPrime(prime, i) == 0) {
-				for (int j = i * i, k = i << 1; j < n; j += k) {
-					makeComposite(prime, j);
-				}
+	
+//	Simple primality test
+	static boolean isPrime(int num) {
+		if (num % 2 == 0) {
+			return false;
+		}
+		int upperLim = (int) Math.sqrt(num);
+		for (int i = 3; i <= upperLim; i += 2) {
+			if (num % i == 0) {
+				return false;
 			}
 		}
-
-		HashSet<Integer> ret = new HashSet<Integer>();
-
-		ret.add(2);
-		for (int i = 3; i <= n; i += 2) {
-			if (ifnotPrime(prime, i) == 0) {
-				ret.add(i);
-			}
-		}
-		return ret;
+		return true;
 	}
 }
