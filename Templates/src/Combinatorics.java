7eb812edbafe400fcb818a60922416570ef4246b
StormFalcon32 <benchen9708@gmail.com>
2018-11-16 16:02:46 -0500

    Changed PascalCase to camelCase

--- Templates/src/Combinatorics.java
+++ Templates/src/Combinatorics.java
@@ -1,6 +1,6 @@
 public class Combinatorics {
 
-	static void CombWithRep(int[] chosen, int[] arr, int currLength, int length, int start, int end) {
+	static void combWithRep(int[] chosen, int[] arr, int currLength, int length, int start, int end) {
 		// Combinations (order doesn't matter 12 is the same as 21) with repeated digits
 		// allowed
 		if (currLength == length) {
@@ -14,12 +14,12 @@ public class Combinatorics {
 
 		for (int i = start; i < end; i++) {
 			chosen[currLength] = i;
-			CombWithRep(chosen, arr, currLength + 1, length, i, end);
+			combWithRep(chosen, arr, currLength + 1, length, i, end);
 		}
 		return;
 	}
 
-	static void MakeAll(int[] chosen, int[] arr, int currLength, int length) {
+	static void makeAll(int[] chosen, int[] arr, int currLength, int length) {
 		// Repeated digits allowed, order matters, all combinations
 		if (currLength == length) {
 			int num = 0;
@@ -32,12 +32,12 @@ public class Combinatorics {
 
 		for (int i = 0; i < arr.length; i++) {
 			chosen[currLength] = i;
-			MakeAll(chosen, arr, currLength + 1, length);
+			makeAll(chosen, arr, currLength + 1, length);
 		}
 		return;
 	}
 
-	static void CombWithoutRep(int[] chosen, int[] arr, int currLength, int length, int start, int end) {
+	static void combWithoutRep(int[] chosen, int[] arr, int currLength, int length, int start, int end) {
 		// Combinations (order doesn't matter 12 is the same as 21) with no repeated
 		// digits allowed
 		if (currLength == length) {
@@ -50,7 +50,7 @@ public class Combinatorics {
 		}
 		for (int i = start; i < end && end - i + 1 > length - currLength; i++) {
 			chosen[currLength] = i;
-			CombWithoutRep(chosen, arr, currLength + 1, length, i + 1, end);
+			combWithoutRep(chosen, arr, currLength + 1, length, i + 1, end);
 		}
 		return;
 	}
